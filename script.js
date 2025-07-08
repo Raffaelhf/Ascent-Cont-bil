a98a1d1b1a3df24bb1d9f33b32d259cc763c11f6
Raffael Freitas <raffaelf94@gmail.com>
2025-07-08 19:51:15 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
- 
+
+
+function destacarCard(id) {
+      // Remove destaque de todos os cards
+      document.querySelectorAll('.card').forEach(card => {
+        card.classList.remove('highlight');
+      });
+
+      // Adiciona destaque ao card clicado
+      const card = document.getElementById(id);
+      card.classList.add('highlight');
+
+      // Remove o destaque após 2 segundos (opcional)
+      setTimeout(() => {
+        card.classList.remove('highlight');
+      }, 2000);
+    }
+
+
+    AOS.init({
+                duration: 1000, // Tempo da animação (1 segundo)
+                easing: "ease-in-out",
+                once: true // A animação acontece apenas uma vez
+            });
+
+            function toggleMenu() {
+                const menu = document.getElementById("menu");
+                menu.classList.toggle("open");
+            }
