7fb7941d690ec7a4b8922bb8984d1bd7ef944a4f
Raffael Freitas <raffaelf94@gmail.com>
2025-07-16 00:17:27 -0300

    final

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 
 function destacarCard(id) {
-      // Remove destaque de todos os cards
+      
       document.querySelectorAll('.card').forEach(card => {
         card.classList.remove('highlight');
       });
 
-      // Adiciona destaque ao card clicado
+      
       const card = document.getElementById(id);
       card.classList.add('highlight');
 
-      // Remove o destaque após 2 segundos (opcional)
+      
       setTimeout(() => {
         card.classList.remove('highlight');
       }, 2000);
@@ -18,12 +18,14 @@ function destacarCard(id) {
 
 
     AOS.init({
-                duration: 1000, // Tempo da animação (1 segundo)
+                duration: 1000, 
                 easing: "ease-in-out",
-                once: true // A animação acontece apenas uma vez
+                once: true 
             });
 
             function toggleMenu() {
                 const menu = document.getElementById("menu");
                 menu.classList.toggle("open");
             }
+
+            
